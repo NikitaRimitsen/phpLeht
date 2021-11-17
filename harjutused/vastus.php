c67d914e0cc36eceb30cc7f3274eae9a764aeefd
Opilane TTHK <opilane@tthk.ee>
2021-11-17 12:00:20 +0200

    Töö

---
+++ harjutused/vastus.php
@@ -0,0 +1,13 @@
+
+<?php
+//lisada info teisest failist
+require_once ('moistatus.php');
+echo "<br> Matemaatika vastus";
+echo "<br>Õiged vastused olid: ";
+echo "arv 1 on ".$arv1;
+echo " ja arv 2 on ".$arv2;
+echo "<br> Linnanimi vastus";
+echo "<br>Õiged vastused olid: ";
+echo "Linn on ".$nimi;
+?>
+
---
+++ navigation.php
@@ -0,0 +1,13 @@
+<nav>
+    <div class="topnav">
+        <a href="?">Kodu</a>
+        <a href="?leht=kontakt">Kontakt</a>
+        <a href="?leht=algus">Harjutused</a>
+        <a href="?leht=moistatus">Tekst funktsioonid</a>
+        <a href="?leht=datefunctions">Aja funktsioonid</a>
+        <a href="?leht=lego">Lego joonis</a>
+        <a href="?leht=pazl">Puzzle</a>
+        <a href="?leht=calculator">Calculator</a>
+        <a href="?leht=pildid">Pildid</a>
+    </div>
+</nav>
